0440551cef9a64e00fbb9221885b37c7f0d2ac39
Loule | Louis <35641311+Loule95450@users.noreply.github.com>
2021-12-16 16:47:26 +0100

    🚶‍♀️ - Addition of the possibility of moving around in the maze

--- src/java/me/loule/maze/controller/MainController.java
+++ src/java/me/loule/maze/controller/MainController.java
@@ -4,6 +4,8 @@ import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.geometry.HPos;
 import javafx.geometry.VPos;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.Pane;
 import me.loule.maze.model.MazeGenerator;
@@ -17,6 +19,18 @@ public class MainController implements Initializable {
     @FXML
     private GridPane mazeGrid;
 
+    @FXML
+    private Button btnRight;
+
+    @FXML
+    private Button btnLeft;
+
+    @FXML
+    private Button btnUp;
+
+    @FXML
+    private Button btnDown;
+
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
         int x = 8;
@@ -24,23 +38,34 @@ public class MainController implements Initializable {
         MazeGenerator maze = new MazeGenerator(x, y);
 
         try {
-            System.out.println("Generating maze...;..");
+            System.out.println("Generating maze...");
             String generatedMaze[] = maze.display().toArray(new String[0]);
 
-//            System.out.println(Arrays.toString(generatedMaze));
+            System.out.println(Arrays.toString(generatedMaze));
 
             int rows = 0;
             int cols = 0;
 
+            int[] playerPosition = {0, 1};
+            System.out.println("Player position: " + playerPosition[0] + ", " + playerPosition[1]);
+
+
+            System.out.println();
+
             for (int i = 0; i < generatedMaze.length; i++) {
-                if (generatedMaze[i].equals("➖➖➖➖") ) {
+                // Bottom row
+                if (generatedMaze[i].equals("➖➖➖➖")) {
                     var myPane = new Pane();
                     myPane.setStyle("-fx-background-color: #000;");
                     myPane.setMaxHeight(2);
                     GridPane.setValignment(myPane, VPos.BOTTOM);
 
                     mazeGrid.add(myPane, cols, rows);
-                } else if (generatedMaze[i].equals("⬛   ") || generatedMaze[i].equals("⬛")) {
+                }
+
+                // Left row
+
+                else if (generatedMaze[i].equals("⬛   ") || generatedMaze[i].equals("⬛")) {
                     var myPane = new Pane();
                     myPane.setStyle("-fx-background-color: #000;");
                     myPane.setMaxWidth(2);
@@ -48,7 +73,10 @@ public class MainController implements Initializable {
                     GridPane.setHalignment(myPane, HPos.LEFT);
                     mazeGrid.add(myPane, cols, rows);
                 }
-                else if(generatedMaze[i].equals("+")  || generatedMaze[i].equals("➖   ")) {
+
+                // Left Center row
+
+                else if (generatedMaze[i].equals("+") || generatedMaze[i].equals("➖   ")) {
                     var myPane = new Pane();
                     myPane.setStyle("-fx-background-color: #000;");
                     myPane.setMaxWidth(2);
@@ -57,6 +85,7 @@ public class MainController implements Initializable {
                     GridPane.setValignment(myPane, VPos.CENTER);
                     mazeGrid.add(myPane, cols, rows);
                 }
+
                 if (cols == 8) {
                     cols = 0;
                     rows++;
@@ -64,6 +93,47 @@ public class MainController implements Initializable {
                     cols++;
 
             }
+
+            mazeGrid.add(new Label("▩"), 0, 1);
+
+            btnRight.setOnMouseClicked(event -> {
+                mazeGrid.getChildren().remove(mazeGrid.getChildren().size() - 1);
+                mazeGrid.add(new Label("▶"), playerPosition[0], playerPosition[1]);
+
+                playerPosition[0]++;
+
+                mazeGrid.add(new Label("▩"), playerPosition[0], playerPosition[1]);
+            });
+
+            btnLeft.setOnMouseClicked(event -> {
+                mazeGrid.getChildren().remove(mazeGrid.getChildren().size() - 1);
+                mazeGrid.add(new Label("◀"), playerPosition[0], playerPosition[1]);
+
+
+                playerPosition[0]--;
+
+                mazeGrid.add(new Label("▩"), playerPosition[0], playerPosition[1]);
+            });
+
+            btnUp.setOnMouseClicked(event -> {
+                mazeGrid.getChildren().remove(mazeGrid.getChildren().size() - 1);
+                mazeGrid.add(new Label("▲"), playerPosition[0], playerPosition[1]);
+
+
+                playerPosition[1]--;
+
+                mazeGrid.add(new Label("▩"), playerPosition[0], playerPosition[1]);
+            });
+
+            btnDown.setOnMouseClicked(event -> {
+                mazeGrid.getChildren().remove(mazeGrid.getChildren().size() - 1);
+                mazeGrid.add(new Label("▼"), playerPosition[0], playerPosition[1]);
+
+
+                playerPosition[1]++;
+
+                mazeGrid.add(new Label("▩"), playerPosition[0], playerPosition[1]);
+            });
         } catch (IOException e) {
             e.printStackTrace();
         }
